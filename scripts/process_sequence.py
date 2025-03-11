b7ee183a11f3982b4dbccd4e1a60b003a78d352c
Thomas Faraut <Thomas.Faraut@gmail.com>
2025-03-11 12:00:22 +0100

    extended information in process_sequence

--- scripts/process_sequence.py
+++ scripts/process_sequence.py
@@ -12,6 +12,10 @@ import orca_predict
 from orca_utils import genomeplot
 from selene_sdk.sequences import Genome
 
+H1_ESC = 0
+HFF = 1
+Cell_Types = {"H1-ESC": H1_ESC, "HFF": HFF}
+
 
 def set_mpos(mpos):
     if mpos == -1:
@@ -30,7 +34,35 @@ def get_sequence(fasta, chrom):
     return sequence
 
 
-def main(fasta, chrom, output_prefix, mpos=-1):
+def dump_target_matrix(predict, output_prefix, mpos, wpos, mutation, chromlen):
+
+    resolutions = ["%dMb" % r for r in [32, 16, 8, 4, 2, 1]]
+
+    # Hff is the second prediction hence 1 in 0-based
+    hff_predictions = predict['predictions'][1]
+    starts = predict['start_coords']
+    ends = predict['end_coords']
+    for pred, resol, start, end in zip(hff_predictions, resolutions, starts, ends):
+        output = "%s_predictions_%s.txt" % (output_prefix, resol)
+        header = ("# Orca=predictions resol=%s mpos=%d wpos=%d start=%d end=%d "
+                  "nbins=250 width=%d chromlen=%d mutation=%s" %
+                  (resol, mpos, wpos,  start, end, end-start, chromlen, mutation))
+        np.savetxt(output, pred, delimiter='\t', header=header, comments='')
+    hff_normmats = predict['normmats'][1]
+    for pred, resol, start, end in zip(hff_normmats, resolutions, starts, ends):
+        output = "%s_normmats_%s.txt" % (output_prefix, resol)
+        header = ("# Orca=normmats resol=%s mpos=%s wpos=%d start=%d end=%d "
+                  "nbins=250 width=%d chromlen=%d mutation=%s" %
+                  (resol, mpos, wpos, start, end, end-start, chromlen, mutation))
+        np.savetxt(output, pred, delimiter='\t', header=header, comments='')
+
+    outputlog = "%s.log" % output_prefix
+    with open(outputlog, "w") as fout:
+        fout.write("# Coordinates of the different matrix in descending order\n")
+        fout.write("%d\t%d\n" % (start, end))
+
+
+def main(fasta, chrom, output_prefix, mutation, mpos=-1):
     """
     """
     sequence = get_sequence(fasta, chrom)
@@ -45,6 +77,12 @@ def main(fasta, chrom, output_prefix, mpos=-1):
                                              mpos=mpos, wpos=midpoint,
                                              use_cuda=True)
 
+    output_pkl = "%s.pkl" % output_prefix
+    file = open(output_pkl, 'wb')
+    pickle.dump(outputs_ref, file)
+    chromlen = 158534110
+    dump_target_matrix(outputs_ref, output_prefix, mpos, wpos, mutation, chromlen)
+
     model_labels = ["H1-ESC", "HFF"]
     genomeplot(
         outputs_ref,
@@ -54,33 +92,6 @@ def main(fasta, chrom, output_prefix, mpos=-1):
         model_labels=model_labels,
         file=output_prefix + ".pdf",
         )
-
-    output_pkl = "%s.pkl" % output_prefix
-    file = open(output_pkl, 'wb')
-    pickle.dump(outputs_ref, file)
-
-    predict = outputs_ref
-    resolutions = ["%dMb" % r for r in [32, 16, 8, 4, 2, 1]]
-
-    # Hff is the second prediction hence 1 in 0-based
-    hff_predictions = predict['predictions'][1]
-    starts = predict['start_coords']
-    ends = predict['end_coords']
-    for pred, resol, start, end in zip(hff_predictions, resolutions, starts, ends):
-        output = "%s_predictions_%s.txt" % (output_prefix, resol)
-        header = ("# Orca=predictions resol=%s start=%d end=%d" % (resol, start, end))
-        np.savetxt(output, pred, delimiter='\t', header=header, comments='')
-    hff_normmats = predict['normmats'][1]
-    for pred, resol, start, end in zip(hff_normmats, resolutions, starts, ends):
-        output = "%s_normmats_%s.txt" % (output_prefix, resol)
-        header = ("# Orca=normmats resol=%s start=%d end=%d" % (resol, start, end))
-        np.savetxt(output, pred, delimiter='\t', header=header, comments='')
-
-    outputlog = "%s.log" % output_prefix
-    with open(outputlog, "w") as fout:
-        fout.write("# Coordinates of the different matrix in descending order\n")
-        for start, end in zip(predict['start_coords'], predict['end_coords']):
-            fout.write("%d\t%d\n" % (start, end))
 
 
 def parse_arguments():
@@ -97,6 +108,8 @@ def parse_arguments():
     parser.add_argument('--mpos',
                         required=False, help='The coordinate to zoom into for multiscale prediction.',
                         default=-1,  type=int)
+    parser.add_argument('--mutation',
+                        required=False, help='The coordinate of the mutated bin.')
 
     args = parser.parse_args()
     return args
@@ -105,4 +118,4 @@ def parse_arguments():
 if __name__ == '__main__':
     args = parse_arguments()
 
-    main(args.fasta, args.chrom, args.outprefix, args.mpos)
+    main(args.fasta, args.chrom, args.outprefix, args.mutation, args.mpos)
